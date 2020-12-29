ca1e155f5683195457bd19565ef349807685f2c5
dannyhannyford <dannyhannyford@gmail.com>
2020-12-28 23:33:40 -0800

    title box

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Container } from '@material-ui/core';
 import '../style.css';
 
 const Home = () => {
   return (
     <div className="home">
-        <h1>danny</h1>
+      <Container maxWidth="md">
+          <div className="homebox">
+            <div className="homecover">
+              <div className="hometxt">
+                <h1>danny</h1>
+                <h3>Software Engineer.</h3>
+              </div>
+            </div>
+          </div>    
+      </Container>
     </div>
   );
 };
