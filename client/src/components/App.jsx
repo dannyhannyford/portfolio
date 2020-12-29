46eded93ea1c2da2adb755b86086f3a5dc22efe5
dannyhannyford <dannyhannyford@gmail.com>
2020-12-28 23:33:12 -0800

    Header component placeholder

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Home from './Home';
 import About from './About';
+import Header from './Header';
 import '../style.css';
 
 const App = () => {
   return (
     <div>
       <Home />
+      <Header />
       <About />
     </div>
     
