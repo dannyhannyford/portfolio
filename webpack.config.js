a7833b95a6028f53a28cc62dc11577566aa4f36a
dannyhannyford <dannyhannyford@gmail.com>
2020-12-28 16:52:30 -0800

    starting files for local react app

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const SRC_FILE = path.resolve(__dirname, 'client', 'src', 'index.jsx');
+const OUT_DIR = path.resolve(__dirname, 'public');
+
+module.exports = {
+  entry: SRC_FILE,
+  output: {
+    path: OUT_DIR,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: 'babel-loader',
+      }
+    ],
+  },
+  mode: 'production',
+  resolve: {
+    fallback: {
+      
+    },
+    extensions: ['.js', '.jsx', '.css'],
+  },
+  watch: false,
+  watchOptions: {
+    aggregateTimeout: 600,
+    poll: 1000,
+    ignored: /node_modules/,
+  },
+};
