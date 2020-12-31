932625ced5b14b57a9d136b441a6ef780e9f775d
dannyhannyford <dannyhannyford@gmail.com>
2020-12-30 16:27:23 -0800

    navbar hides when scrolls, has buttons for places on the page, not interactive atm

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,8 +1,78 @@
 import React from 'react';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import BrightnessHighIcon from '@material-ui/icons/BrightnessHigh';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Slide from '@material-ui/core/Slide';
+import IconButton from '@material-ui/core/IconButton';
 
-const Nav = () => {
+
+function HideOnScroll(props) {
+  const { children } = props;
+  const trigger = useScrollTrigger();
+
   return (
-    <div>Nav</div>
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+const theme = createMuiTheme({
+  palette: {
+    secondary: {
+      main: '#faaa8f'
+    },
+  },
+});
+
+const Nav = (props) => {
+  
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <HideOnScroll {...props}>
+          <AppBar
+          color="secondary"
+          >
+            <Toolbar>
+              <IconButton
+              edge="start"
+              color="inherit"
+              >  
+                <BrightnessHighIcon />
+              </IconButton>
+              <Typography variant="h6">
+                dannyhanny
+              </Typography>
+                <Button 
+                color="inherit"
+                >
+                  Home
+                </Button>
+                <Button 
+                color="inherit"
+                >
+                  About
+                </Button>
+                <Button 
+                color="inherit"
+                >
+                  Portfolio
+                </Button>
+                <Button 
+                color="inherit"
+                >
+                  Contact
+                </Button>
+            </Toolbar>
+          </AppBar>
+        </HideOnScroll>
+      </ThemeProvider>
+    </>
   );
 };
 
