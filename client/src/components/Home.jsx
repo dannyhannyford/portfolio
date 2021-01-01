a55f229ab64dcb7464d0f88af3bcb01ef27e505a
dannyhannyford <dannyhannyford@gmail.com>
2020-12-31 18:33:01 -0800

    took out paper

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
 import IconLabelButtons from './IconLabelButtons';
+import Box from '@material-ui/core/Box';
 import '../style.css';
 
 // TODO how to keep my box within the background
 
 const Home = () => {
   return (
-    <div id="home" className="home">
-      <Container maxWidth="md">
-          <div className="homebox pad">
-            <div className="homecover">
-              <div className="hometxt">
-                <h1>danny</h1>
-                <h3>Software Engineer. I work with beeps and boops.</h3>
-                <h3>Looking for a team that will push me towards new horizons.</h3>
+    <Box height="100%">
+      <div id="home" className="home">
+        <Container maxWidth="md">
+            <div className="homebox pad">
+              <div className="homecover">
+                <div className="hometxt">
+                  <h1>danny</h1>
+                  <h3>Full-Stack Software Engineer. I work with beeps and boops.</h3>
+                </div>
               </div>
             </div>
-          </div>
-          <IconLabelButtons /> 
-      </Container>
-    </div>
+            <IconLabelButtons /> 
+        </Container>
+      </div>
+    </Box>
   );
 };
 
