16b1541edec499d1c8cc7c5150089afec437348a
dannyhannyford <dannyhannyford@gmail.com>
2021-01-03 17:40:25 -0800

    object for holding my application info

---
+++ client/src/components/Utils.js
@@ -0,0 +1,35 @@
+const applications = [];
+
+// Paste-E
+// Tensor Tango
+// krak-o-lantern
+// souperhost-3000
+
+// title
+// description
+// link
+// pic
+
+applications.push({
+  title: 'souperhost:3000',
+  description: 'Travel booking application component displays more places to stay',
+  link: '',
+  pic: '',
+}, {
+  title: 'Tensor Tango',
+  description: 'Tensorflow.js application using a camera to track and label dance moves',
+  link: '',
+  pic: '',
+}, {
+  title: 'Krak-O-Lantern',
+  description: 'Travel booking application built to withstand traffic spikes',
+  link: '',
+  pic: '',
+}, {
+  title: 'Paste-E-Bot',
+  description: 'Discord bot for playing music and other server commands',
+  link: '',
+  pic: '',
+})
+
+export default applications;
