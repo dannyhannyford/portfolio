d0d07a2aaa557522ec41b3a13e3a447c7d700ec3
dannyhannyford <dannyhannyford@gmail.com>
2021-01-15 23:22:53 -0800

    background image added here

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { withStyles } from '@material-ui/core/styles';
 import Home from './Home';
 import About from './About';
 import Nav from './Nav';
@@ -6,8 +8,27 @@ import Portfolio from './Portfolio';
 import Contact from './Contact';
 import '../style.css';
 
+const styles = () => ({
+  '@global': {
+    '#home': {
+      backgroundImage: 'url(https://portfoliodan.s3-us-west-2.amazonaws.com/oceanBeach3.webp)',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center center',
+      backgroundSize: 'cover',
+      height: '90vh',
+    },
+    html: {
+      height: '100%',
+    },
+    '#app': {
+      height: '100%',
+    },
+  },
+});
+
 const App = () => (
   <div>
+    <CssBaseline />
     <Nav />
     <Home />
     <About />
@@ -16,4 +37,4 @@ const App = () => (
   </div>
 );
 
-export default App;
+export default withStyles(styles)(App);
